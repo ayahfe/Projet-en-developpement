2abb609e16cebfa6e53e13a0bf82596a9bfe8f21
ayahfe <imaya48484@gmail.com>
2025-10-29 20:14:51 +0100

    [UPD]Modification - Modification du rendez-vous

--- Projet-en-developpement-aya/src/App.jsx
+++ Projet-en-developpement-aya/src/App.jsx
@@ -5,15 +5,17 @@ import RootLayout from "./frontend/rootLayout/RootLayout";
 import Login from "./frontend/login/Login";
 import Signup from "./frontend/signup/Signup";
 import ProduitList from "./frontend/produitCard/ProduitCard.jsx";
+import RendezVous from "./frontend/rdv/rdv.jsx"; // 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     children: [
-      { index: true, element: <ProduitList /> },   // <-- page d’accueil
+      { index: true, element: <ProduitList /> },
       { path: "login", element: <Login /> },
-      { path: "signup", element: <Signup /> }
+      { path: "signup", element: <Signup /> },
+      { path: "rendezvous", element: <RendezVous /> },
     ],
   },
 ]);
