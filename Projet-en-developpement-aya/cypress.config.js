14724cb24ea0f9fd0dcfe32f79213f4889393007
Areej Budayr <2397186@cmontmorency.qc.ca>
2025-12-13 14:10:40 -0500

    feat: ajout de l’API signup + tests Cypress pour l’inscription utilisateur

--- Projet-en-developpement-aya/cypress.config.js
+++ Projet-en-developpement-aya/cypress.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    specPattern: ['cypress/e2e/**/*.cy.js', 'src/backend/controlleurs/**/*.test.js'],  // Moved outside setupNodeEvents
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
