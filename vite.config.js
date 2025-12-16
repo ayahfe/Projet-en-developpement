65759bf48577e893561a9728db29f2a876130390
ayahfe <167936209+ayahfe@users.noreply.github.com>
2025-12-16 16:58:08 -0500

    [Test] modiification de vite.config.js pour GitHub actions
    Added configuration options for Vitest to include and exclude specific test files.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteImagemin from 'vite-plugin-imagemin';
 
+// Configuration de Vite
 export default defineConfig({
   plugins: [
     react(),
@@ -10,10 +11,13 @@ export default defineConfig({
       avif: { quality: 50 },
     }),
   ],
-  
+
+  // Configuration de Vitest pour les tests
   test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./vitest.setup.js",
+    environment: "jsdom", // Utilisation de jsdom pour simuler le DOM
+    globals: true,         // Activation des fonctions globales comme `expect`
+    setupFiles: "./vitest.setup.js", // Fichier de configuration avant les tests
+    include: ["src/**/*.{test,spec}.{js,ts,tsx}"], // Où Vitest cherche les fichiers de test
+    exclude: ["node_modules/**/*"], // Exclure les fichiers dans node_modules
   },
 });
