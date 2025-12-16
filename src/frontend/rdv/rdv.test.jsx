69d329bd493b72004536286d7454ce1c866b1de8
ahafiane <ahafiane44@gmail.com>
2025-12-16 14:52:11 -0500

    [TEST] Deuxième test d'integration

--- src/frontend/rdv/rdv.test.jsx
+++ src/frontend/rdv/rdv.test.jsx
@@ -245,7 +245,7 @@ it("affiche un rendez-vous annulé avec la bonne classe", async () => {
 
   expect(card.closest(".apt-card")).toHaveClass("cancelled");
 });
-
+//TEST INTEGRATION 1
 it("permet à un utilisateur connecté d'ouvrir le formulaire de création", async () => {
   mockUseAuth.mockReturnValue({ user: { id: 1 } });
 
@@ -256,6 +256,16 @@ it("permet à un utilisateur connecté d'ouvrir le formulaire de création", asy
   expect(
     await screen.findByText(/Nouveau Rendez-vous/i)
   ).toBeInTheDocument();
+});
+//TEST INTEGRATION 2
+it("empêche un utilisateur non connecté de créer un rendez-vous", async () => {
+  mockUseAuth.mockReturnValue({ user: null });
+
+  render(<CalendrierRdv />);
+
+  fireEvent.click(screen.getByText(/Nouveau RDV/i));
+
+  expect(global.alert).toHaveBeenCalled();
 });
 
   
